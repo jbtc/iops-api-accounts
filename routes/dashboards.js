ef2ebef3c9afe1d0bffe82f7a0d57e59c75262be
Tyler Garlick <tjgarlick@gmail.com>
2016-02-15 20:27:49 -0700

    Added roles to user

--- routes/dashboards.js
+++ routes/dashboards.js
@@ -6,29 +6,19 @@ import Joi from '../lib/joi';
 import * as Models from '../lib/models';
 import _ from 'lodash';
 
-const VERSION = `/v1`;
-
-const PREFIX = {
-  ACCOUNT: `${VERSION}/users/{userId}`
-};
-
-const PATH = {
-  ACCOUNT_CLAIMS: `${PREFIX.ACCOUNT}/dashboards`
-};
-
 export default [
 
   {
-    path: `${VERSION}/dashboards`,
+    path: `/v1/dashboards`,
     method: 'GET',
     config: {
       tags: ['api'],
-      description: `Dashboards`,
+      description: `Get active dashboards`,
 
       handler: {
         async: async (request, reply) => {
           try {
-            const results = Services.dashboards.find({isActive: true});
+            const results = await Services.dashboards.find({ isActive: true });
             return reply(results);
           } catch (e) {
             return reply(e);
@@ -39,17 +29,17 @@ export default [
   },
 
   {
-    path: `${VERSION}/dashboards/{dashboardId}`,
+    path: `/v1/dashboards/{dashboardId}`,
     method: 'GET',
     config: {
       tags: ['api'],
-      description: `Dashboard`,
+      description: `Get a Dashboard`,
 
       handler: {
         async: async (request, reply) => {
           const id = request.params.dashboardId;
           try {
-            let result = Services.dashboards.findById(id);
+            let result = await Services.dashboards.findById(id);
             return reply(result);
           } catch (e) {
             return reply(e);
@@ -60,15 +50,15 @@ export default [
   },
 
   {
-    path: `${VERSION}/dashboards/{dashboardId}`,
+    path: `/v1/dashboards/{dashboardId}`,
     method: 'PUT',
     config: {
       tags: ['api'],
-      description: `Dashboard`,
+      description: `Update a Dashboard`,
 
       validate: {
-        params: {dashboardId: Joi.shortid().required()},
-        payload: Joi.object(_.omit(Models.Dashboard, 'userId')).meta({className: 'UpdateDashboard'})
+        params: { dashboardId: Joi.shortid().required() },
+        payload: Joi.object(_.omit(Models.Dashboard, 'userId')).meta({ className: 'UpdateDashboard' })
       },
 
       handler: {
@@ -76,7 +66,7 @@ export default [
           const id = request.params.dashboardId;
           const dashboard = request.payload;
           try {
-            let result = Services.dashboards.update(id, dashboard);
+            let result = await Services.dashboards.update(id, dashboard);
             return reply(result);
           } catch (e) {
             return reply(e);
@@ -87,14 +77,14 @@ export default [
   },
 
   {
-    path: `${VERSION}/dashboards/{dashboardId}`,
+    path: `/v1/dashboards/{dashboardId}`,
     method: 'DELETE',
     config: {
       tags: ['api'],
       description: `Remove Dashboard`,
 
       validate: {
-        params: {dashboardId: Joi.shortid().required()}
+        params: { dashboardId: Joi.shortid().required() }
       },
 
       handler: {
@@ -112,14 +102,14 @@ export default [
   },
 
   {
-    path: PATH.ACCOUNT_CLAIMS,
+    path: '/v1/users/{userId}/dashboards',
     method: 'GET',
     config: {
       tags: ['api'],
       description: `User's Dashboard`,
 
       validate: {
-        params: {userId: Joi.shortid().required()}
+        params: { userId: Joi.shortid().required() }
       },
 
       handler: {
@@ -127,7 +117,7 @@ export default [
           const userId = request.params.userId;
           try {
 
-            const results = Services.dashboards.find({isActive: true, userId});
+            const results = await Services.dashboards.find({ isActive: true, userId });
             return reply(results);
           } catch (e) {
             return reply(e);
@@ -138,24 +128,24 @@ export default [
   },
 
   {
-    path: PATH.ACCOUNT_CLAIMS,
+    path: '/v1/users/{userId}/dashboards',
     method: 'POST',
     config: {
       tags: ['api'],
-      description: `Create Dashboard`,
+      description: `Create a User's Dashboard`,
 
       validate: {
-        params: {userId: Joi.shortid().required()},
-        payload: Joi.object(_.omit(Models.Dashboard, 'isActive')).meta({className: 'NewDashboard'})
+        params: { userId: Joi.shortid().required() },
+        payload: Joi.object(_.omit(Models.Dashboard, 'isActive')).meta({ className: 'NewDashboard' })
       },
 
       handler: {
         async: async (request, reply) => {
           const userId = request.params.userId;
-          let dashboard = _.merge(request.payload, {userId});
+          let dashboard = _.merge(request.payload, { userId });
 
           try {
-            const result = Services.dashboards.create(dashboard);
+            const result = await Services.dashboards.create(dashboard);
             return reply(result);
           } catch (e) {
             return reply(e);
@@ -163,7 +153,73 @@ export default [
         }
       }
     }
-  }
+  },
+
+  {
+    path: '/v1/users/{userId}/dashboards/{dashboardId}',
+    method: 'PUT',
+    config: {
+      tags: ['api'],
+      description: `Update User's Dashboard`,
+
+      validate: {
+        params: {
+          userId: Joi.shortid().required(),
+          dashboardId: Joi.shortid().required()
+        },
+        payload: Joi.object(Models.Dashboard).meta({ className: 'UpdateDashboard' })
+      },
+
+      handler: {
+        async: async (request, reply) => {
+          const userId = request.params.userId;
+          const dashboardId = request.params.dashboardId;
+
+          let dashboard = _.merge(request.payload, { userId });
+
+          if (dashboard._id) {
+            delete dashboard._id;
+          }
+
+          try {
+            const result = await Services.dashboards.update(dashboardId, dashboard);
+            return reply(result);
+          } catch (e) {
+            return reply(e);
+          }
+        }
+      }
+    }
+  },
+
+  {
+    path: '/v1/users/{userId}/dashboards/{dashboardId}',
+    method: 'DELETE',
+    config: {
+      tags: ['api'],
+      description: `Delete a User's Dashboard`,
+
+      validate: {
+        params: {
+          userId: Joi.shortid().required(),
+          dashboardId: Joi.shortid().required()
+        }
+      },
+
+      handler: {
+        async: async (request, reply) => {
+          const dashboardId = request.params.dashboardId;
+
+          try {
+            const result = await Services.dashboards.remove(dashboardId);
+            return reply(result);
+          } catch (e) {
+            return reply(e);
+          }
+        }
+      }
+    }
+  },
 
 
 ];
